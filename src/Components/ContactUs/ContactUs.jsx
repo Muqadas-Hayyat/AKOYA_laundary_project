3f5cec8283f6cd9433586acbcb91bb2b8902b6db
muqadashayyat8@gmail.com <muqadashayyat8@gmail.com>
2025-09-05 10:08:18 +0500

    update contact page

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,45 +1,55 @@
-import React from "react";
-import ContactLeftCard from "./ContactLeftCArd";
-import ContactRightCard from "./ContactRightCard";
+import React from 'react'
+import ContactRightCard from './ContactRightCard'
+import ContactLeftCard from './LeftCard'
 
 const ContactUs = () => {
   return (
     <section
-      id="contact"
-      className="relative bg-[#f8f5f2] py-24 px-6 md:px-16 lg:px-24 overflow-hidden"
+      id='contact'
+      className='relative bg-[#f8f5f2] py-24 px-6 md:px-16 lg:px-24 overflow-hidden'
     >
-      
-
-      <div className="max-w-7xl mx-auto relative">
+      <div className='max-w-7xl mx-auto relative'>
         {/* Section Heading */}
-        <div className="text-center mb-20">
-          <h2 className="text-3xl md:text-4xl font-light text-[#1C1C1C] mb-3 tracking-tight" data-aos="fade-up" data-aos-dealy="0">
+        <div className='text-center mb-20'>
+          <h2
+            className='text-3xl md:text-4xl font-light text-[#1C1C1C] mb-3 tracking-tight'
+            data-aos='fade-up'
+            data-aos-dealy='0'
+          >
             Contact Us
           </h2>
-          <div className="flex justify-center items-center">
-            <div className="w-12 h-px bg-[#D4AF37] mx-4" data-aos="fade-right" data-aos-dealy="300"></div>
-            <p className="text-lg text-[#D4AF37] tracking-widest font-medium" data-aos="fade-up" data-aos-dealy="300">
+          <div className='flex justify-center items-center'>
+            <div
+              className='w-12 h-px bg-[#D4AF37] mx-4'
+              data-aos='fade-right'
+              data-aos-dealy='300'
+            ></div>
+            <p
+              className='text-lg text-[#D4AF37] tracking-widest font-medium'
+              data-aos='fade-up'
+              data-aos-dealy='300'
+            >
               GET IN TOUCH
             </p>
-            <div className="w-12 h-px bg-[#D4AF37] mx-4" data-aos="fade-left" data-aos-dealy="300"></div>
+            <div
+              className='w-12 h-px bg-[#D4AF37] mx-4'
+              data-aos='fade-left'
+              data-aos-dealy='300'
+            ></div>
           </div>
         </div>
-        </div>
+      </div>
 
+      {/* Grid Layout */}
+      <div className='grid md:grid-cols-2 gap-12 lg:gap-16'>
+        {/* Contact Information */}
+        <ContactLeftCard />
 
-       
-
-        {/* Grid Layout */}
-        <div className="grid md:grid-cols-2 gap-12 lg:gap-16">
-          {/* Contact Information */}
-          <ContactLeftCard/>
-
-          {/* Contact Form */}
-         <ContactRightCard/>
-        </div>
-      
+        {/* Contact Form */}
+        <ContactRightCard />
+      </div>
     </section>
-  );
-};
+  )
+}
 
-export default ContactUs;
+export default ContactUs
